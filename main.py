cd8920384a986571de48dfcafdf8bd7aec0b6ae4
yuan9011 <139258651+yuan9011@users.noreply.github.com>
2023-08-24 14:43:42 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,248 @@
+import pygame
+import random
+import os
+
+FPS = 60
+WIDTH = 500
+HEIGHT = 600
+
+# 顏色
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# 遊戲初始化 and 創建視窗
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('第一個遊戲')
+clock = pygame.time.Clock()
+
+# 載入圖片
+background_img = pygame.image.load(os.path.join('img', 'background.png')).convert()
+player_img = pygame.image.load(os.path.join('img', 'player.png')).convert()
+bullet_img = pygame.image.load(os.path.join('img', 'bullet.png')).convert()
+
+rock_imgs = []
+
+for i in range(7):
+    rock_imgs.append(pygame.image.load(os.path.join('img', f'rock{i}.png')).convert())
+
+# 載入字體
+font_name = pygame.font.match_font('arial')
+
+# 文字寫入畫面
+def draw_text(surf, text, size, x, y):
+    
+    # 創建文字
+    font = pygame.font.Font(font_name, size)
+    
+    # 渲染文字
+    text_surface = font.render(text, True, WHITE)
+    
+    # 定位文字
+    text_rect = text_surface.get_rect()
+    text_rect.centerx = x
+    text_rect.top = y
+    
+    # 畫出文字
+    surf.blit(text_surface, text_rect)
+    
+
+# 操控 sprite
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(player_img, (50, 38))
+        
+        # 圖片背景透明化
+        self.image.set_colorkey(BLACK)
+        
+        # 定位圖片
+        self.rect = self.image.get_rect()
+        
+        # 以圖片中心點畫一個圓
+        self.radius = 20
+        
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 8
+
+    def update(self):
+        key_pressed = pygame.key.get_pressed()
+        
+        if key_pressed[pygame.K_a]:
+            self.rect.x -= self.speedx
+        
+        if key_pressed[pygame.K_d]:
+            self.rect.x += self.speedx
+        
+        # 判斷圖片是否超出畫面
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+            
+        if self.rect.left < 0:
+            self.rect.left = 0
+            
+    def shoot(self):
+        
+        # 創建子彈
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
+
+# 石頭 sprite
+class Rock(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image_ori = random.choice(rock_imgs)
+        
+        # 圖片背景透明化
+        self.image_ori.set_colorkey(BLACK)
+        
+        self.image = self.image_ori.copy()
+        
+        # 定位圖片
+        self.rect = self.image.get_rect()
+        
+        # 以圖片中心點畫一個圓
+        self.radius = int(self.rect.width * 0.85 / 2)
+        
+        self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-180, -100)
+        self.speedx = random.randrange(-3, 3)
+        self.speedy = random.randrange(2, 5)
+        
+        # 總轉動度數
+        self.total_degree = 0
+        
+        self.rot_degree = random.randrange(-3, 3)
+        
+    def rotate(self):
+        self.total_degree += self.rot_degree
+        self.total_degree %= 360
+        self.image = pygame.transform.rotate(self.image_ori, self.total_degree)
+        
+        # 轉動前圖片中心點
+        center = self.rect.center
+        
+        # 重新定位轉動後圖片
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+    
+    def update(self):
+        self.rotate()
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        
+        # 判斷圖片是否超出畫面
+        if self.rect.y > HEIGHT \
+            or self.rect.left > WIDTH \
+            or self.rect.right < 0:
+            
+            # 重置圖片
+            self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedx = random.randrange(-3, 3)
+            self.speedy = random.randrange(2, 10)
+
+
+# 子彈 sprite
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullet_img
+        
+        # 圖片背景透明化
+        self.image.set_colorkey(BLACK)
+        
+        # 定位圖片
+        self.rect = self.image.get_rect()
+        
+        self.rect.centerx = x
+        self.rect.y = y
+        self.speedy = -10
+        
+    def update(self):
+        self.rect.y += self.speedy
+        
+        # 判斷圖片是否超出畫面
+        if self.rect.bottom < 0:
+            
+            # 刪除圖片
+            self.kill()
+
+
+all_sprites = pygame.sprite.Group()
+rocks = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+
+player = Player()
+all_sprites.add(player)
+
+for _ in range(8):
+    rock = Rock()
+    all_sprites.add(rock)
+    rocks.add(rock)
+    
+# 分數
+score = 0
+
+running = True
+
+# 遊戲迴圈
+while running:
+    clock.tick(FPS)
+    
+    # 取得輸入
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            running = False
+        
+        # 判斷是否按下鍵盤鍵
+        elif event.type == pygame.KEYDOWN:
+            
+            # 按下空白鍵呼叫shoot
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+    
+    # 更新遊戲
+    all_sprites.update()
+    
+    # rocks & bullets 碰撞處理
+    hits = pygame.sprite.groupcollide(rocks, bullets, True, True)
+    
+    # 判斷石頭 & 子彈是否碰撞
+    for hit in hits:
+        score += hit.radius
+        
+        # 創建石頭
+        r = Rock()
+        all_sprites.add(r)
+        rocks.add(r)
+    
+    # player & rocks 碰撞處理
+    # 加強碰撞判斷(矩形 -> 圓形)
+    hits = pygame.sprite.spritecollide(player, rocks, False, pygame.sprite.collide_circle)
+    
+    # 判斷飛船 & 石頭是否碰撞
+    if hits:
+        running = False
+    
+    # 畫面顯示
+    screen.fill(BLACK)
+    
+    # 畫面背景
+    screen.blit(background_img, (0, 0))
+    
+    all_sprites.draw(screen)
+    
+    # 顯示文字
+    draw_text(screen, str(score), 18, WIDTH / 2, 10)
+    
+    pygame.display.update()
+    
+pygame.quit()
